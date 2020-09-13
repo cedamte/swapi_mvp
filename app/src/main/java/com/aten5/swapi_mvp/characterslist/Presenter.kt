87ab5e8e21972dd81d474883d0faa84034a56b16
dami <dami4_u@msn.com>
2020-09-14 00:41:47 +0100

    Setup presenter class

---
+++ app/src/main/java/com/aten5/swapi_mvp/characterslist/Presenter.kt
@@ -0,0 +1,25 @@
+package com.aten5.swapi_mvp.characterslist
+
+import com.aten5.swapi_mvp.model.DataSource
+import com.aten5.swapi_mvp.model.RetrofitClient
+import com.aten5.swapi_mvp.model.data.ResultData
+
+class Presenter(
+    private val view: CharactersContract.View
+) : CharactersContract.Presenter, DataSource.Listener {
+
+    private val dataSource: DataSource = RetrofitClient(this)
+
+    override fun getData() {
+        dataSource.getCharactersList()
+        view.showLoading()
+    }
+
+    override fun onSuccess(data: ResultData) {
+        view.showResult(result = data)
+    }
+
+    override fun onFailure(throwable: Throwable) {
+        view.showError(throwable.message)
+    }
+}

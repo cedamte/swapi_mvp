bda56cf6e5a13b9682f458bdea39c76850942ed7
dami <dami4_u@msn.com>
2020-09-14 00:57:28 +0100

    Set up RecyclerView Adapter

--- app/src/main/java/com/aten5/swapi_mvp/characterslist/MainActivity.kt
+++ app/src/main/java/com/aten5/swapi_mvp/characterslist/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.aten5.swapi_mvp.characterslist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aten5.swapi_mvp.R
 import com.aten5.swapi_mvp.databinding.ActivityMainBinding
+import com.aten5.swapi_mvp.model.data.Characters
 import com.aten5.swapi_mvp.model.data.ResultData
 
 class MainActivity : AppCompatActivity(), CharactersContract.View {
     private lateinit var binding: ActivityMainBinding
+
+    private val viewAdapter = CharactersAdapter()
+    private val presenter = Presenter(this)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +25,10 @@ class MainActivity : AppCompatActivity(), CharactersContract.View {
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             addItemDecoration(DividerItemDecoration(context, LinearLayoutManager.VERTICAL))
+            adapter = viewAdapter
         }
+
+        presenter.getData()
     }
 
     override fun showLoading() {
@@ -30,6 +38,11 @@ class MainActivity : AppCompatActivity(), CharactersContract.View {
     }
 
     override fun showResult(result: ResultData) {
+        val newList = result as Characters
+
+        // Update RecyclerView Data
+        viewAdapter.upDateList(newList = newList.results)
+
         binding.loading.visibility = View.GONE
         binding.recyclerView.visibility = View.VISIBLE
     }
